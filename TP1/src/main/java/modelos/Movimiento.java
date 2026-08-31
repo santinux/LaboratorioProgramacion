dfd6c69595f91d658cf93990d68d8ef570ea70b7
santinux <santinofuentes24.sf@gmail.com>
2026-08-30 21:09:27 -0300

    Add: clases para manejar los movimientos (transacciones bancarias)

---
+++ TP1/src/main/java/modelos/Movimiento.java
@@ -0,0 +1,35 @@
+package modelos;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Movimiento
+{
+        private int id;
+        private int idClienteOrigen;
+        private int idCuentaOrigen;
+        private int idClienteDestino;
+        private int idCuentaDestino;
+        private LocalDateTime timestamp;
+        private BigDecimal monto;
+        private TipoMovimiento tipo;
+        
+        public Movimiento(int id, int idClienteOrigen, int idCuentaOrigen,
+                          int idClienteDestino, int idCuentaDestino,
+                          LocalDateTime timestamp, BigDecimal monto, TipoMovimiento tipo)
+        {
+                this.id = id;
+                this.idClienteOrigen = idClienteOrigen;
+                this.idCuentaOrigen = idCuentaOrigen;
+                this.idClienteDestino = idClienteDestino;
+                this.idCuentaDestino = idCuentaDestino;
+                this.timestamp = timestamp;
+                this.monto = monto;
+                this.tipo = tipo;
+        }
+        
+        public BigDecimal getMonto()
+        {
+                return (this.monto);
+        }
+}
---
+++ TP1/src/main/java/modelos/TipoMovimiento.java
@@ -0,0 +1,7 @@
+package modelos;
+
+public enum TipoMovimiento
+{
+        INGRESO,
+        EGRESO
+}
