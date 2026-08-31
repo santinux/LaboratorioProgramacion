c41ba57940e3f20dd9d0308f2eb3ccc956eae51c
santinux <santinofuentes24.sf@gmail.com>
2026-08-30 21:08:25 -0300

    Fix: cambios menores en la definicion de las clases

--- TP1/src/main/java/modelos/Cliente.java
+++ TP1/src/main/java/modelos/Cliente.java
@@ -2,29 +2,15 @@ package modelos;
 
 public class Cliente extends Usuario
 {
-        private Cliente(byte tipoUsuario, short id, int nroDNI, String nombre, String apellido)
+        public Cliente(int id, int nroDNI, String nombre, String apellido)
         {
-                super(tipoUsuario, id, nroDNI, nombre, apellido);
+                super((byte) 1, id, nroDNI, nombre, apellido);
         }
         
-        /**
-         * Constructor de clase.
-         * Crea y retorna un nuevo cliente.
-         *
-         * @param nroDNI
-         * @param nombre
-         * @param apellido
-         * @return
-         */
-        public static Cliente crearCliente(int nroDNI, String nombre, String apellido)
+        @Override
+        public void setId(int id)
         {
-                short id = buscarUltimoIDEnDB();
-                return new Cliente((byte) 0, id, nroDNI, nombre, apellido);
-        }
-        
-        private static short buscarUltimoIDEnDB()
-        {
-                return 0;
+                super.setId(id);
         }
         
         @Override
--- TP1/src/main/java/modelos/Usuario.java
+++ TP1/src/main/java/modelos/Usuario.java
@@ -3,18 +3,23 @@ package modelos;
 public abstract class Usuario
 {
         private byte tipoUsuario;
-        private short id;
+        private int id;
         private int nroDNI;
         private String nombre;
         private String apellido;
         
-        protected Usuario(byte tipoUsuario, short id, int nroDNI, String nombre, String apellido)
+        protected Usuario(byte tipoUsuario, int id, int nroDNI, String nombre, String apellido)
         {
                 this.tipoUsuario = tipoUsuario;
                 this.id = id;
                 this.nroDNI = nroDNI;
                 this.nombre = nombre;
                 this.apellido = apellido;
+        }
+        
+        protected void setId(int id)
+        {
+                this.id = id;
         }
         
         protected abstract byte getTipoUsuario();
