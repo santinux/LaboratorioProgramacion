9d79a944aead12b4bb2b5f54e0875f4daaf64c5a
santinux <santinofuentes24.sf@gmail.com>
2026-09-01 13:00:31 -0300

    Add: mejora en la creacion de la vista principal en Main

--- TP1/src/main/java/Main.java
+++ TP1/src/main/java/Main.java
@@ -1,10 +1,14 @@
 import vistas.VistaPrincipal;
 
+import javax.swing.*;
+
 public class Main
 {
         public static void main(String[] args)
         {
-                VistaPrincipal vistaPrincipal = new VistaPrincipal();
-                vistaPrincipal.setVisible(true);
+                SwingUtilities.invokeLater(() -> {
+                        VistaPrincipal vistaPrincipal = new VistaPrincipal();
+                        vistaPrincipal.setVisible(true);
+                });
         }
 }
